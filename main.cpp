edfe37c2581272c21f03b9b711cb9dee2206ca50
pdetoni <pdpereira2003@gmail.com>
2024-03-02 19:16:17 -0300

    adicionando funcao srand com uso da funcao time

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-const char input[] = "abcdefghijklmnopqrstuvwxyz"
-                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                     "0123456789"
-                     ".,:;!?()[]{}<>+-*/=|&^%$#@~_";
+const char input[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:;!?()[]{}<>+-*/=|&^%$#@~_";
 
 int main(){
         int n;
+        unsigned seed = time(0);
+        srand(seed);
 
         cout << "Insira a quantidade de caracteres da senha: ";
         cin >> n;
-        
+
         cout << "Senha: ";
         for(int i = 0; i < n; i++){
             cout << input[rand() % (sizeof(input) - 1)];
         }
 
-
-        cout<<RAND_MAX<<endl;
 
         return 0;
     }
